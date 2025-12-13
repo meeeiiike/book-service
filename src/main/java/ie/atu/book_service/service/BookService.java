c6cb64aac8438e9b7736ea81adaffdade4b52605
Emre <g00427391@atu.ie>
2025-12-13 23:54:33 +0000

    env file created, docker container works properly.

--- src/main/java/ie/atu/book_service/service/BookService.java
+++ src/main/java/ie/atu/book_service/service/BookService.java
@@ -13,6 +13,7 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
